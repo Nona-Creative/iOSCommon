d947e3d79d52b08bdb9299a45fa702be772ced8b
David O'Reilly <dave@breakdesign.com>
2017-01-23 11:45:02 +0200

    Add extension to grayscale image.

---
+++ UIImage+Gray.swift
@@ -0,0 +1,22 @@
+//
+//  UIImage+Gray.swift
+//  orderin
+//
+//  Created by David O'Reilly on 2017/01/23.
+//  Copyright © 2017 Breakdesign. All rights reserved.
+//
+
+import Foundation
+
+extension UIImage {
+    
+    func grayScaled() -> UIImage {
+        let currentFilter = CIFilter(name: "CIPhotoEffectNoir")
+        currentFilter!.setValue(CIImage(image: self), forKey: kCIInputImageKey)
+        let output = currentFilter!.outputImage
+        let context = CIContext(options: nil)
+        let cgimg = context.createCGImage(output!,from: output!.extent)
+        let processedImage = UIImage(cgImage: cgimg!)
+        return processedImage
+    }
+}
