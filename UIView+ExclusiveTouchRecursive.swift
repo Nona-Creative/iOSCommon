627b55485304984cb260b4391fac08e9d4bdc355
David O'Reilly <dave@outlandium.com>
2017-03-24 16:06:31 +0200

    Add recursive isExclusiveTouch setting

---
+++ UIView+ExclusiveTouchRecursive.swift
@@ -0,0 +1,18 @@
+//
+//  UIView+ExclusiveTouchRecursive.swift
+//  orderin
+//
+//  Created by David O'Reilly on 2017/03/24.
+//  Copyright © 2017 Breakdesign. All rights reserved.
+//
+
+import Foundation
+
+extension UIView {
+    func setExclusiveTouchRecursive(_ exclusive: Bool) {
+        for subView in subviews {
+            subView.setExclusiveTouchRecursive(exclusive)
+            subView.isExclusiveTouch = true
+        }
+    }
+}
