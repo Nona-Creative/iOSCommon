a692855b10bc2027a0658b43ec8b3c8283f9561e
David O'Reilly <dave@outlandium.com>
2017-04-11 12:30:38 +0200

    Add workaround for iOS 8 crash

--- UIImage+Gray.swift
+++ UIImage+Gray.swift
@@ -11,12 +11,16 @@ import Foundation
 extension UIImage {
 
     func grayScaled() -> UIImage {
-        let currentFilter = CIFilter(name: "CIPhotoEffectNoir")
-        currentFilter!.setValue(CIImage(image: self), forKey: kCIInputImageKey)
-        let output = currentFilter!.outputImage
-        let context = CIContext(options: nil)
-        let cgimg = context.createCGImage(output!, from: output!.extent)
-        let processedImage = UIImage(cgImage: cgimg!)
-        return processedImage
+        if #available(iOS 9.0, *) { // Workaround for iOS 8 bug, creating the context crashes
+            let currentFilter = CIFilter(name: "CIPhotoEffectNoir")
+            currentFilter!.setValue(CIImage(image: self), forKey: kCIInputImageKey)
+            let output = currentFilter!.outputImage
+            let context = CIContext(options: nil)
+            let cgimg = context.createCGImage(output!, from: output!.extent)
+            let processedImage = UIImage(cgImage: cgimg!)
+            return processedImage
+        } else {
+            return self
+        }
     }
 }
